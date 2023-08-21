66c9246eaeb3c282487191c6f5730dba44868d79
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-08-21 22:36:49 +0300

    Older version completed

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,25 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self):
+        self._name = name
+        self._breed = breed
+
+    def get_name(self):
+        return self._name
+    
+    def get_breed(self):
+        return self._breed
+    
+    def set_name(self, name):
+        if(type(name)== str and (1 >= name <= 25)):
+            self._name = name
+        else:
+            return 'Name must be string between 1 and 25 characters.'
+    name = property(get_name, set_name)
+
+    def set_breed(self, breed):
+        if(breed in APPROVED_BREEDS):
+            self._breed = breed
+        else:
+            return 'The breed must be in the following list of dog breeds: '+ str(APPROVED_BREEDS)
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,25 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self):
+        self._name = name
+        self._job = job
+
+    def get_name(self):
+        return self._name
+    
+    def get_job(self):
+        return self._job
+    
+    def set_name(self, name):
+        if(type(name)== str and (name < 25)):
+            self._name = name.title()
+        else:
+            return 'Name must be string under 25 characters.'
+    name = property(get_name, set_name)
+
+    def set_job(self, job):
+        if(job in APPROVED_JOBS):
+            self._job = job
+        else:
+            return 'The job must be in the following list of jobs: '+ str(APPROVED_JOBS)
